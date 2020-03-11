e4184751c851a39d3404354b58d81c0ff2773cd8
ahsan-tariq343 <58512603+ahsan-tariq343@users.noreply.github.com>
2020-03-11 14:02:24 +0500

    loader fixes

--- app/screens/Diary/index.js
+++ app/screens/Diary/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { Ionicons, EvilIcons } from '@expo/vector-icons';
 import DiaryTile from '../../components/DiaryTile'
-import Loader from '../../components/Loader'
+import Loader from '../../components/loader'
 import CalendarComponent from '../../components/CalendarComponent'
 import data from '../../StaticData'
 import _ from 'underscore';

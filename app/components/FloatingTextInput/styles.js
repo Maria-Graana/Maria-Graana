1ae9a0bc73b598ee64ff7536f5175430741b9413
Haris Amjad <53810039+haris-0525@users.noreply.github.com>
2020-03-18 14:31:06 +0500

    update styles.js

--- app/components/FloatingTextInput/styles.js
+++ app/components/FloatingTextInput/styles.js
@@ -6,13 +6,13 @@ export default StyleSheet.create({
         borderRadius: 4,
         borderColor:AppStyles.colors.primaryColor,
         backgroundColor: '#ffffff',
-        paddingHorizontal: 8,
-        paddingBottom:8,
+        paddingHorizontal: 16,
+        paddingBottom:4,
         marginHorizontal: 12
     },
     labelStyle:{
         fontSize:AppStyles.fontSize.medium,
         fontFamily:AppStyles.fonts.defaultFont,
-        paddingHorizontal: 8,
+        paddingHorizontal: 16,
     }
 })

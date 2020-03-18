5e28a23d931a7e87b1192026127ddeaa127ea160
ahsan-tariq343 <58512603+ahsan-tariq343@users.noreply.github.com>
2020-03-18 12:23:43 +0500

    new screen roles added

--- app/hoc/role.js
+++ app/hoc/role.js
@@ -1,10 +1,25 @@
 const role = {
-    diary: {
+    Diary: {
         roles: [
-            { role: 'sub admin 1', access: { v: true, a: true, e: true, d: false} },
-            { role: 'sub admin 2', access: {v: true, a: false, e: false, d: false} },
+            { role: 'sub_admin 1', access: { v: true, a: true, e: false, d: false} },
+            { role: 'sub_admin 2', access: { v: true, a: true, e: false, d: false} },
         ],
     },
+    Inventory: {
+        roles: [
+            { role: 'sub_admin 1', access: { v: true, a: true, e: true, d: true} },
+            { role: 'sub_admin 2', access: { v: true, a: true, e: true, d: true} },
+            { role: 'sub_admin 4', access: { v: true, a: true, e: true, d: true} },
+        ],
+    },
+    TeamDiary: {
+        roles: [
+            { role: 'admin 1', access: { v: true, a: false, e: false, d: false} },
+            { role: 'admin 2', access: { v: true, a: false, e: false, d: false} },
+            { role: 'admin 3', access: { v: true, a: false, e: false, d: false} },
+            { role: 'sub_admin 1', access: { v: true, a: true, e: false, d: false} },
+        ],
+    }
 }
 
 module.exports = role;

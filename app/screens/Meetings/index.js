32999f535b074fcc27f8d71444f73e56aa6bf1e1
umergraana <awanumer69@gmail.com>
2020-03-31 15:23:21 +0500

    feb added

--- app/screens/Meetings/index.js
+++ app/screens/Meetings/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, KeyboardAvoidingView, ScrollView } from 'react-native';
-import { Text } from 'native-base';
+import { Text, Fab } from 'native-base';
+import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios'
 import { connect } from 'react-redux';
 import * as RootNavigation from '../../navigation/RootNavigation';
@@ -8,12 +9,22 @@ import * as RootNavigation from '../../navigation/RootNavigation';
 class Meetings extends Component {
   constructor(props) {
     super(props)
-
+    this.state = {
+      active: false,
+    }
   }
   render() {
     return (
       <View>
         <Text>Meetings</Text>
+        <Fab
+						active={this.state.active}
+						direction="up"
+						style={{ backgroundColor: '#0D73EE' }}
+						position="topRight"
+						onPress={() => this.setState({ active: !this.state.active })}>
+						<Ionicons name="md-add" color="#ffffff" />
+					</Fab>
       </View>
     )
   }

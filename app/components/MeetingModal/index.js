308e6de9db3a24e428009b61c05273392d989166
umergraana <awanumer69@gmail.com>
2020-03-31 17:38:37 +0500

    add meeting form completed

--- app/components/MeetingModal/index.js
+++ app/components/MeetingModal/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import { Button } from 'native-base';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
+import { Button, } from 'native-base';
 import styles from './style'
 import AppStyles from '../../AppStyles'
 import Modal from 'react-native-modal';
+import DateComponent from '../../components/DatePicker'
+import times from '../../../assets/img/times.png'
 
 class MeetingModal extends React.Component {
   constructor(props) {
@@ -11,12 +13,40 @@ class MeetingModal extends React.Component {
   }
 
   render() {
-    const { openModal, active } = this.props
+    const { openModal, active, handleForm, formSubmit } = this.props
     return (
       <Modal isVisible={active}>
         <View style={[styles.modalMain]}>
-          <Text>Hello!</Text>
-          <Button title="Hide modal" onPress={() => { openModal() }} />
+          <TouchableOpacity  style={styles.timesBtn}  onPress={() => { openModal() }}>
+            <Image source={times} style={styles.timesImg} />
+          </TouchableOpacity>
+
+          <View style={[styles.formMain]}>
+
+            {/* **************************************** */}
+            <View style={[AppStyles.mainInputWrap]}>
+              <View style={[AppStyles.inputWrap]}>
+                <DateComponent date={''} mode='date' placeholder='Select Start Time' onTimeChange={(value) => this.handleForm(value, 'startTime')} />
+              </View>
+            </View>
+
+            {/* **************************************** */}
+            <View style={[AppStyles.mainInputWrap]}>
+              <View style={[AppStyles.inputWrap]}>
+                <DateComponent date={''} mode='time' placeholder='Select Start Time' onTimeChange={(value) => this.handleForm(value, 'startTime')} />
+              </View>
+            </View>
+
+            {/* **************************************** */}
+            <View style={[AppStyles.mainInputWrap]}>
+              <Button
+                onPress={() => { openModal() }}
+                style={[AppStyles.formBtn, styles.addInvenBtn]}>
+                <Text style={AppStyles.btnText}>ADD Meeting</Text>
+              </Button>
+            </View>
+
+          </View>
         </View>
       </Modal>
     )
--- app/components/MeetingModal/style.js
+++ app/components/MeetingModal/style.js
@@ -4,13 +4,28 @@ import AppStyles from '../../AppStyles'
 export default styles = StyleSheet.create({
   modalMain: {
     backgroundColor: '#E8EDF0',
-    minHeight: '60%',
     borderRadius: 10,
     padding: 15,
+    paddingTop: 50,
+    paddingBottom: 50,
     zIndex: 5,
+    position: 'relative',
     elevation: 5,
     shadowOffset: { width: 5, height: 5 },
     shadowColor: '#33333312',
     shadowOpacity: 1,
-  }
+  },
+  addInvenBtn: {
+    marginTop: 50,
+  },
+  timesBtn: {
+    position: 'absolute',
+    right: 15,
+    top: 15,
+  },
+  timesImg: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
 });

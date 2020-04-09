d508034b4270e0fb5b38ed49947337189d3d43ce
Haris Amjad <53810039+haris-0525@users.noreply.github.com>
2020-04-09 12:59:02 +0500

    Added Change Password screen to stack and drawer

---
+++ app/screens/ChangePassword/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import AppStyles from '../../AppStyles';
+
+class ChangePassword extends Component {
+    render() {
+        return (
+            <View style={AppStyles.container}>
+                <Text> ChangePassword </Text>
+            </View>
+        )
+    }
+}
+
+export default ChangePassword;

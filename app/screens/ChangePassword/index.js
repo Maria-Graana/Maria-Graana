5dfb2051f0137ac12d49bda546d5a454b9544f53
Haris Amjad <53810039+haris-0525@users.noreply.github.com>
2020-04-09 13:51:46 +0500

    change password initial working

--- app/screens/ChangePassword/index.js
+++ app/screens/ChangePassword/index.js
@@ -1,15 +1,125 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, Alert } from 'react-native'
+import { connect } from 'react-redux';
+import { Button } from 'native-base'
 import AppStyles from '../../AppStyles';
+import axios from 'axios'
+import ErrorMessage from '../../components/ErrorMessage'
+import helper from '../../helper'
 
 class ChangePassword extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkValidation: false,
+            formData: {
+                oldPassword: '',
+                newPassword: '',
+                confirmNewPassword: '',
+            }
+        }
+    }
+
+    handleForm = (value, name) => {
+        const { formData } = this.state
+        formData[name] = value
+        this.setState({ formData });
+    }
+
+    formSubmit = () => {
+        const { formData } = this.state;
+        const { oldPassword, newPassword, confirmNewPassword } = formData;
+        if (!oldPassword || !newPassword || !confirmNewPassword) {
+            this.setState({
+                checkValidation: true
+            })
+        } else {
+            this.changePassword(formData)
+        }
+
+    }
+
+    changePassword = (data) => {
+        if (data.newPassword !== data.confirmNewPassword) {
+            alert('Please make sure new password and confirm password are same!')
+            return;
+        }
+
+        const body = {
+            password: data.newPassword,
+            oldPassword: data.oldPassword
+        }
+
+        console.log(body);
+
+        axios.patch(`/api/user/resetpassword`,body).then(response => {
+            console.log(response);
+            if (response.data === 'incorrect old password') {
+                helper.errorToast('Incorrect old password entered!')
+            }
+            else {
+                helper.successToast('Password Changed Successfully!')
+            }
+
+        }).catch(error => {
+            console.log(error);
+            helper.errorToast('Something went wrong, please try again!')
+        })
+    }
+
+
     render() {
+        const { checkValidation, formData } = this.state;
+        const { oldPassword, newPassword, confirmNewPassword } = formData;
+
         return (
             <View style={AppStyles.container}>
-                <Text> ChangePassword </Text>
-            </View>
+                <View style={[AppStyles.mainInputWrap]}>
+                    <View style={[AppStyles.inputWrap]}>
+                        <TextInput style={[AppStyles.formControl, AppStyles.inputPadLeft, AppStyles.formFontSettings]} secureTextEntry={true} placeholder={'Current Password'} onChangeText={(text) => this.handleForm(text, 'oldPassword')} />
+                    </View>
+                    {
+                        checkValidation === true && oldPassword === '' && <ErrorMessage errorMessage={'Required'} />
+                    }
+                </View>
+
+                <View style={[AppStyles.mainInputWrap]}>
+                    <View style={[AppStyles.inputWrap]}>
+                        <TextInput style={[AppStyles.formControl, AppStyles.inputPadLeft, AppStyles.formFontSettings]} secureTextEntry={true} placeholder={'New Password'} onChangeText={(text) => this.handleForm(text, 'newPassword')} />
+                    </View>
+                    {
+                        checkValidation === true && newPassword === '' && <ErrorMessage errorMessage={'Required'} />
+                    }
+                </View>
+
+                <View style={[AppStyles.mainInputWrap]}>
+                    <View style={[AppStyles.inputWrap]}>
+                        <TextInput style={[AppStyles.formControl, AppStyles.inputPadLeft, AppStyles.formFontSettings]} secureTextEntry={true} placeholder={'Confirm new password'} onChangeText={(text) => this.handleForm(text, 'confirmNewPassword')} />
+                    </View>
+                    {
+                        checkValidation === true && confirmNewPassword === '' && <ErrorMessage errorMessage={'Required'} />
+                    }
+                </View>
+
+                <View style={{ marginVertical: 10 }}>
+                    <Button onPress={() => { this.formSubmit() }}
+                        style={[AppStyles.formBtn]}>
+                        <Text style={AppStyles.btnText}>CHANGE PASSWORD</Text>
+                    </Button>
+                </View>
+
+            </View >
+
         )
     }
 }
 
-export default ChangePassword;
+mapStateToProps = (store) => {
+    return {
+        user: store.user.user
+    }
+}
+
+export default connect(mapStateToProps)(ChangePassword)
+

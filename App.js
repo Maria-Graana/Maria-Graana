0e305f0dd99f15d6ada59ba0495ab2b48154425c
ahsan-tariq343 <58512603+ahsan-tariq343@users.noreply.github.com>
2020-04-11 01:22:37 +0500

    new changes added

--- App.js
+++ App.js
@@ -9,7 +9,17 @@ import { PersistGate } from 'redux-persist/integration/react';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { SplashScreen } from 'expo';
+import * as Sentry from 'sentry-expo';
+import * as firebase from 'firebase';
+import Constants from 'expo-constants';
 
+const firebaseConfig = {
+	apiKey: "AIzaSyBcMF6jv0j0EY82JC9XW0jKMu4o7fRDKrg",
+	authDomain: "graana-push-notification-e4375.firebaseapp.com",
+	databaseURL: "https://graana-push-notification-e4375.firebaseio.com",
+	storageBucket: "graana-push-notification-e4375.appspot.com",
+	measurementId: "G-8CRWYW95PT"
+  }
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -21,6 +31,14 @@ export default class App extends React.Component {
 
 	async componentDidMount() {
 		SplashScreen.preventAutoHide();
+		firebase.initializeApp(firebaseConfig)
+		Sentry.init({
+			dsn: 'https://d23d9b7296fa43a1ab41150269693c2f@o375514.ingest.sentry.io/5195102',
+			enableInExpoDevelopment: true,
+			debug: true
+		})
+		Sentry.setRelease(Constants.manifest.revisionId);
+
 		await Font.loadAsync({
 			Roboto: require('native-base/Fonts/Roboto.ttf'),
 			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
@@ -36,7 +54,7 @@ export default class App extends React.Component {
 	render() {
 		if (!this.state.isReady) {
 			return <AppLoading />;
-		  }
+		}
 		return (
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>

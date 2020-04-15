32f0933dd546aae188b3a047795d501ef78f54f0
umergraana <awanumer69@gmail.com>
2020-04-15 15:06:19 +0500

    lead created

--- app/components/MeetingTile/index.js
+++ app/components/MeetingTile/index.js
@@ -12,16 +12,15 @@ class MeetingTile extends React.Component {
   }
 
   render() {
-    const { data, openStatus, doneStatusId, doneStatus, sendStatus, editFunction } = this.props
+    const { data, openStatus, editFunction } = this.props
     let taskTypeData = []
     data.taskType === 'meeting' ?
       taskTypeData = StaticData.meetingStatus
       :
       taskTypeData = StaticData.callStatus
-      let zindex = doneStatus === true && doneStatusId == data.id && styles.tileIndex
     return (
-      <TouchableOpacity style={[zindex]} onPress={() => {data.taskType === 'meeting' && editFunction(data.id)}}>
-        <View style={[styles.mainTileView, doneStatus === true && doneStatusId == data.id && styles.tileIndex]}>
+      <TouchableOpacity onPress={() => {data.taskType === 'meeting' && editFunction(data.id)}}>
+        <View style={[styles.mainTileView,]}>
           <View style={[styles.contentView, AppStyles.flexDirectionRow]}>
             <View style={styles.border}>
               <Text style={[AppStyles.mrTen, styles.meetingCon]}>{data.taskType} @</Text>
@@ -38,23 +37,9 @@ class MeetingTile extends React.Component {
                 data.taskType === 'meeting' &&
                 <Text style={[styles.doneText]}>{data.status}</Text>
               }
-              <TouchableOpacity style={[styles.doneBtn]} onPress={() => { openStatus(data.id) }}>
+              <TouchableOpacity style={[styles.doneBtn]} onPress={() => { openStatus(data) }}>
                 <Image source={dots} style={styles.dotsImg} />
               </TouchableOpacity>
-              {
-                doneStatus === true && doneStatusId == data.id &&
-                <View style={[styles.dropDownMain]}>
-                  {
-                    taskTypeData.map((item, key) => {
-                      return (
-                        <TouchableOpacity style={[styles.doneBtnBottom]} onPress={() => { sendStatus(item.value) }} key={key}>
-                          <Text style={styles.blueColor}>{item.name}</Text>
-                        </TouchableOpacity>
-                      )
-                    })
-                  }
-                </View>
-              }
             </View>
           </View>
         </View>

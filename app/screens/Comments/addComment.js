2f3ee30a3bdd8fe600074df717a424071b5ecaa3
Haris Amjad <53810039+haris-0525@users.noreply.github.com>
2020-04-15 21:06:11 +0500

    fixes in comment

--- app/screens/Comments/addComment.js
+++ app/screens/Comments/addComment.js
@@ -11,7 +11,11 @@ const AddComment = (props) => {
         <View style={styles.container}>
             <Textarea
                 placeholderTextColor="#bfbbbb"
-                style={[AppStyles.formControl, AppStyles.formFontSettings, { height: 150, paddingRight: 20 }]} rowSpan={10}
+                style={[AppStyles.formControl, AppStyles.formFontSettings, { height: 150, paddingRight: 20,  
+                    elevation: 10,
+                    shadowOffset: { width: 5, height: 5 },
+                    shadowColor: 'lightgrey',
+                    shadowOpacity: 1, }]} rowSpan={10}
                 placeholder="Add Comment"
                 onChangeText={(text) => setComment(text)}
                 value={comment}

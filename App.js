83378a8b0b5eb2503e5694c5e36de7ed8bfb9424
Yasser Zubair <yasserzubair68@gmail.com>
2020-04-19 18:42:03 +0500

    touchable opcity active opacity set golbally

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import RootStack from './app/navigation/AppNavigation'
-import { AppLoading } from 'expo';
-import { Root } from "native-base";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { store, persistor } from './app/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider as PaperProvider } from 'react-native-paper';
-
 import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { SplashScreen } from 'expo';
 import * as Sentry from 'sentry-expo';
+
+import { persistor, store } from './app/store';
+
+import { AppLoading } from 'expo';
 // import * as firebase from 'firebase';
 import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { Root } from "native-base";
+import RootStack from './app/navigation/AppNavigation'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SplashScreen } from 'expo';
+import { setCustomTouchableOpacity } from 'react-native-global-props'
 
 // const firebaseConfig = {
 // 	apiKey: "AIzaSyBcMF6jv0j0EY82JC9XW0jKMu4o7fRDKrg",
@@ -32,6 +34,7 @@ export default class App extends React.Component {
 	}
 
 	async componentDidMount() {
+		setCustomTouchableOpacity({activeOpacity:0.8})
 		SplashScreen.preventAutoHide();
 		// firebase.initializeApp(firebaseConfig)
 		Sentry.init({

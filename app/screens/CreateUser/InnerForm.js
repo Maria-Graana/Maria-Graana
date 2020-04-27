c6bcb2ce2c6ffb7d3f1f30a8714df8b21db5e836
umergraana <awanumer69@gmail.com>
2020-04-27 15:07:14 +0500

    changes

--- app/screens/CreateUser/InnerForm.js
+++ app/screens/CreateUser/InnerForm.js
@@ -136,7 +136,7 @@ class InnerForm extends Component {
         {/* **************************************** */}
         <View style={[AppStyles.mainInputWrap,]}>
           <View style={[AppStyles.inputWrap]}>
-            <TextInput style={[AppStyles.formControl, styles.padLeft]} value={getRoles.subRole} editable={false} />
+            <TextInput style={[AppStyles.formControl, styles.padLeft]} value={getRoles.subRole === 'area_manager' && 'Area Manager'} editable={false} />
           </View>
         </View>
 

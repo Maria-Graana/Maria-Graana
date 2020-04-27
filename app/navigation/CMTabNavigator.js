b9d2c68af9e8b1dc5b02af117a9bfd63d083d4d7
Summer Kiflain <summersyed@yahoo.com>
2020-04-27 15:35:23 +0500

    Fixed TabNavigator tabs width for smaller screens on CM Lead

--- app/navigation/CMTabNavigator.js
+++ app/navigation/CMTabNavigator.js
@@ -3,6 +3,8 @@ import Meetings from '../screens/Meetings/index';
 import Payments from '../screens/Payments/index';
 import React from 'react';
 import styles from './style';
+import { Dimensions } from 'react-native'
+const { width } = Dimensions.get('window')
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -12,14 +14,14 @@ export default function CMLeadTabs() {
             tabBarOptions={{
                 scrollEnabled: true,
                 labelStyle: { fontSize: 12 },
-                tabStyle: { width: 206 },
+                tabStyle: { width: width / 2, paddingLeft: 0, paddingRight: 0 },
                 indicatorStyle: {
                     borderBottomColor: '#ffffff',
                     borderBottomWidth: 2,
                 }
                 // style: { shadowColor: 'transparent', elevation: 0, borderTopColor: "transparent", borderTopWidth: 0 },
             }}>
-            <Tab.Screen name="Meetings" options={{ title: 'Calls/Meetings' }} component={Meetings} />
+            <Tab.Screen name="Meetings" options={{ title: 'Calls / Meetings' }} component={Meetings} />
             <Tab.Screen name="Payments" component={Payments} />
         </Tab.Navigator>
     );

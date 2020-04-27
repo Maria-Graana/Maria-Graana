bde042eeb02f07e01a5b85975645e9ff5257585f
Haris Amjad <53810039+haris-0525@users.noreply.github.com>
2020-04-27 16:35:19 +0500

    tab bar changes

--- app/navigation/CMTabNavigator.js
+++ app/navigation/CMTabNavigator.js
@@ -5,6 +5,7 @@ import React from 'react';
 import styles from './style';
 import { Dimensions } from 'react-native'
 const { width } = Dimensions.get('window')
+import AppStyles from '../AppStyles'
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -12,13 +13,15 @@ export default function CMLeadTabs() {
     return (
         <Tab.Navigator
             tabBarOptions={{
-                scrollEnabled: true,
-                labelStyle: { fontSize: 12 },
+                scrollEnabled: false,
+                labelStyle: { fontSize: 12, fontFamily:AppStyles.fonts.semiBoldFont },
+                activeTintColor: AppStyles.colors.primaryColor,
+                inactiveTintColor: AppStyles.colors.subTextColor,
                 tabStyle: { width: width / 2, paddingLeft: 0, paddingRight: 0 },
                 indicatorStyle: {
                     borderBottomColor: '#ffffff',
                     borderBottomWidth: 2,
-                }
+                },
                 // style: { shadowColor: 'transparent', elevation: 0, borderTopColor: "transparent", borderTopWidth: 0 },
             }}>
             <Tab.Screen name="Meetings" options={{ title: 'Calls / Meetings' }} component={Meetings} />

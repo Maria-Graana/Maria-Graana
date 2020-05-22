0c4adc450b28f51f794c10ebe95678ccdd1c3e59
Haris Amjad <harisamjad25@gmail.com>
2020-05-22 12:18:52 +0500

    made custom component for scroll end loader

---
+++ app/components/OnLoadMoreComponent/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, ActivityIndicator} from 'react-native'
+import styles from './styles'
+
+const OnLoadMoreComponent = props => {
+    return (
+        props.onEndReached ?
+        <View style={[props.styles, styles.rowStyle]}>
+        <ActivityIndicator  size='small' color={AppStyles.colors.primaryColor}/>
+        </View> 
+        :
+         null
+    )
+}
+
+export default OnLoadMoreComponent

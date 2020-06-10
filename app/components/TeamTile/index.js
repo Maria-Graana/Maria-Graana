003afbf0e02a25678552c9e7a5bec2b4c1424e85
Haris Amjad <harisamjad25@gmail.com>
2020-06-10 16:43:52 +0500

    team diary initial enhancements completed

--- app/components/TeamTile/index.js
+++ app/components/TeamTile/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import AppStyles from '../../AppStyles';
 import Avatar from '../../components/Avatar/index';
+import helper from '../../helper'
 
 class TeamTile extends React.Component {
     constructor(props) {
@@ -10,7 +11,8 @@ class TeamTile extends React.Component {
     render() {
         const { selected, data, selectedId } = this.props;
         const { item } = data;
-
+        const organizationName = item.organization && item.organization.name ? item.organization.name : '';
+        console.log(item);
         return (
             <TouchableOpacity
                 activeOpacity={.7}
@@ -20,17 +22,29 @@ class TeamTile extends React.Component {
                     <View style={{ paddingRight: 10, }}>
                         <Avatar data={item} />
                     </View>
-                    <View style={{ flexDirection: 'column' }}>
-                        <View style={{ paddingTop: 5 }}>
-                            <Text style={[styles.textFont, { fontSize: 15 }]}>
-                                {item.firstName} {item.lastName}
-                            </Text>
+                    <View style={{ flexDirection: 'column', flex: 1, justifyContent: 'center' }}>
+                        <View style={{ flexDirection: 'row', width: '100%', marginBottom: 5, alignItems: 'center' }}>
+                            <View style={{ flexDirection: 'row', width: '80%', alignItems: 'center' }}>
+                                <Text style={[styles.textFont, { fontSize: 15, marginRight: 10 }]}>
+                                    {item.firstName} {item.lastName}
+                                </Text>
+                                <Text style={[styles.textFont, { fontSize: 12, color: AppStyles.colors.subTextColor }]}>
+                                    {item.armsUserRole && item.armsUserRole.subRole}
+                                </Text>
+                            </View>
+                            {
+                                organizationName !== '' ? <Text style={[styles.textFont, {
+                                    fontSize: 18, color: organizationName ===
+                                        'Graana' ? 'red' : 'green' , width: '20%', textAlign: 'right'
+                                }]}>
+                                    {organizationName !== '' ? organizationName === 'Graana' ? 'G' : 'A' : ''}
+                                </Text> : null}
+
                         </View>
-                        <View style={{ paddingTop: 5 }}>
-                            <Text style={[styles.textFont, { fontSize: 12, color: AppStyles.colors.subTextColor }]}>
-                                {item.armsUserRole && item.armsUserRole.subRole}
-                            </Text>
-                        </View>
+                        {item.zone && <Text style={[styles.textFont, { fontSize: 12, color: AppStyles.colors.subTextColor }]}>
+                            {item.zone.zone_name ? helper.capitalize(item.zone.zone_name) : ''}
+                        </Text>}
+
                     </View>
                 </View>
                 <View style={styles.underLine}
@@ -42,14 +56,16 @@ class TeamTile extends React.Component {
 
 const styles = StyleSheet.create({
     simpleStyle: {
+        flex: 1,
         flexDirection: 'row',
-        paddingVertical:10,
-        paddingHorizontal:5
+        paddingVertical: 10,
+        marginHorizontal: 10,
     },
     selectedStyle: {
+        flex: 1,
         flexDirection: 'row',
-        paddingVertical:10,
-        paddingHorizontal:5,
+        paddingVertical: 10,
+        marginHorizontal: 10,
         backgroundColor: AppStyles.colors.backgroundColor
     },
     textFont: {

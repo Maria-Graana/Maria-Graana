10516c3c8d17f141901864989c40a65fd351aa33
Haris Amjad <harisamjad25@gmail.com>
2020-08-06 15:13:57 +0500

    fixed bugs and added new enhauncements to search in leads

--- app/screens/RentLeads/index.js
+++ app/screens/RentLeads/index.js
@@ -52,14 +52,14 @@ class RentLeads extends React.Component {
 	onFocus = async () => {
 		const statusValue = await getItem('statusFilterRent');
 		if (statusValue) {
-			this.setState({ statusFilter: String(statusValue) },()=>{
-				this.fetchLeads(this.state.statusFilter)
+			this.setState({ statusFilter: String(statusValue) }, () => {
+				this.fetchLeads()
 			})
 		}
 		else {
 			storeItem('statusFilterRent', 'all');
-			this.setState({ statusFilter: 'all' },()=>{
-				this.fetchLeads(this.state.statusFilter)
+			this.setState({ statusFilter: 'all' }, () => {
+				this.fetchLeads()
 			})
 		}
 	}
@@ -71,14 +71,15 @@ class RentLeads extends React.Component {
 		})
 	}
 
-	fetchLeads = (statusFilter) => {
-		const { sort, pageSize, page, leadsData, showSearchBar, searchText } = this.state
+	fetchLeads = () => {
+		const { sort, pageSize, page, leadsData, showSearchBar, searchText, statusFilter } = this.state
 		this.setState({ loading: true })
 		let query = ``
 		if (showSearchBar && searchText !== '') {
 			query = `/api/leads?purpose=rent&searchBy=name&q=${searchText}&pageSize=${pageSize}&page=${page}`
 		}
 		else {
+			console.log('statusFilter', statusFilter, sort, page, pageSize)
 			query = `/api/leads?purpose=rent&status=${statusFilter}${sort}&pageSize=${pageSize}&page=${page}`
 		}
 		axios.get(`${query}`)
@@ -106,7 +107,7 @@ class RentLeads extends React.Component {
 		this.clearStateValues()
 		this.setState({ statusFilter: status, leadsData: [] }, () => {
 			storeItem('statusFilterRent', status);
-			this.fetchLeads(this.state.statusFilter);
+			this.fetchLeads();
 		})
 	}
 
@@ -155,7 +156,7 @@ class RentLeads extends React.Component {
 	}
 
 	sendStatus = (status) => {
-		this.setState({ sort: status, activeSortModal: !this.state.activeSortModal }, () => { this.fetchLeads(this.state.statusFilter); })
+		this.setState({ sort: status, activeSortModal: !this.state.activeSortModal }, () => { this.fetchLeads(); })
 	}
 
 	openStatus = () => {
@@ -169,7 +170,7 @@ class RentLeads extends React.Component {
 	clearAndCloseSearch = () => {
 		this.setState({ searchText: '', showSearchBar: false, sort: '&order=Desc&field=createdAt' }, () => {
 			this.clearStateValues();
-			this.fetchLeads('all')
+			this.fetchLeads()
 		})
 	}
 
@@ -195,19 +196,17 @@ class RentLeads extends React.Component {
 					{
 						showSearchBar ? <View style={[styles.filterRow, { paddingBottom: 0, paddingTop: 0, paddingLeft: 0 }]}>
 							<Search
-								containerWidth="80%"
+								containerWidth="100%"
 								placeholder='Search leads here'
 								searchText={searchText}
 								setSearchText={(value) => this.setState({ searchText: value })}
 								showShadow={false}
 								showClearButton={true}
+								returnKeyType={'search'}
+								onSubmitEditing={() => this.fetchLeads()}
+								autoFocus={true}
 								closeSearchBar={() => this.clearAndCloseSearch()}
 							/>
-							<TouchableOpacity onPress={() => this.fetchLeads(null)}
-								style={styles.roundButtonView}
-								activeOpacity={0.6}>
-								<Text style={[AppStyles.btnText, { fontSize: 12 }]}>Search</Text>
-							</TouchableOpacity>
 						</View>
 							:
 							<View style={styles.filterRow}>
@@ -258,7 +257,7 @@ class RentLeads extends React.Component {
 										page: this.state.page + 1,
 										onEndReachedLoader: true
 									}, () => {
-										this.fetchLeads(statusFilter);
+										this.fetchLeads();
 									});
 								}
 							}}

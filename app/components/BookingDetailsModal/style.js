0612bfdc28f330a22d720b20a6ae275de5c86884
Umer farooq <45381610+umergraana@users.noreply.github.com>
2020-10-16 05:50:48 -0500

    unit details modal css

---
+++ app/components/BookingDetailsModal/style.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native';
+import AppStyles from '../../AppStyles'
+
+export default styles = StyleSheet.create({
+  modalMain: {
+    backgroundColor: '#fff',
+    // borderRadius: 10,
+    padding: 15,
+    paddingTop: 30,
+    paddingBottom: 50,
+    zIndex: 5,
+    position: 'relative',
+    elevation: 5,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: '#33333312',
+    shadowOpacity: 1,
+  },
+  timesBtn: {
+    position: 'absolute',
+    right: 15,
+    top: 15,
+  },
+  timesImg: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
+  MainTileView: {
+    borderBottomWidth: 1,
+    borderColor: '#ECECEC',
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  smallText: {
+    color: '#1F2029',
+    fontSize: 14,
+    marginBottom: 3,
+    textTransform: 'capitalize',
+  },
+  largeText: {
+    color: '#1F2029',
+    fontSize: 20,
+  },
+  noBorder: {
+    borderBottomWidth: 0
+  },
+});

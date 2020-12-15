9bee0bc3ee78890bf622a636c35f5fc71509c5e7
Umer farooq <45381610+umergraana@users.noreply.github.com>
2020-12-15 14:38:28 +0500

    tile screen done

---
+++ app/screens/AssignAreas/index.js
@@ -0,0 +1,82 @@
+import * as RootNavigation from '../../navigation/RootNavigation'
+import { Alert, FlatList, Image, Text, View } from 'react-native'
+import Ability from '../../hoc/Ability'
+import AppStyles from '../../AppStyles'
+import React from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import helper from '../../helper'
+import styles from './style'
+import AssignAreasTile from '../../components/AssignAreasTile'
+import Loader from '../../components/loader'
+
+
+class AssignAreas extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			assignedAreas: [],
+			loading: false
+		}
+	}
+
+	componentDidMount() {
+		this.fetchAssignedAreas();
+	}
+
+	fetchAssignedAreas = () => {
+		const { user } = this.props
+		const { loading } = this.state
+		this.setState({
+			loading: true
+		})
+		axios.get(`/api/user/userareas?userId=${user.id}`)
+			.then((res) => {
+				this.setState({
+					assignedAreas: res.data,
+					loading: false,
+				})
+			})
+	}
+
+	render() {
+		const { assignedAreas, loading } = this.state
+		return (
+			<View style={styles.mainContainer}>
+				<View style={styles.scrollView}>
+
+					{
+						assignedAreas &&
+							assignedAreas.length > 0 &&
+							loading === false
+							?
+							< FlatList
+								data={assignedAreas}
+								renderItem={({ item, index }) => (
+									<AssignAreasTile
+										data={item}
+										index={index}
+										loading={loading}
+									/>
+								)}
+							/>
+							: 
+
+						<Loader loading={loading}/>
+					}
+
+				</View>
+			</View>
+		)
+	}
+}
+
+mapStateToProps = (store) => {
+	return {
+		user: store.user.user,
+		contacts: store.contacts.contacts,
+	}
+}
+
+export default connect(mapStateToProps)(AssignAreas)
---
+++ app/screens/AssignAreas/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+export default styles = StyleSheet.create({
+    mainContainer: {
+        padding: 10,
+    },
+    scrollView: {
+        height: '100%'
+    },
+});

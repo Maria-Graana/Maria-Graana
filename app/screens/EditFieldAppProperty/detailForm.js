71422bef00242aebc709be7165c7b2d2b3a2cad1
haris0525 <haris.amjad@graana.com>
2021-01-04 12:31:02 +0500

    updated text

--- app/screens/EditFieldAppProperty/detailForm.js
+++ app/screens/EditFieldAppProperty/detailForm.js
@@ -368,7 +368,7 @@ class DetailForm extends Component {
             containerStyle={[AppStyles.mainInputWrap, styles.geotagButton, { width: '100%' }]}
             containerBackgroundColor={'white'}
             textColor={AppStyles.colors.primaryColor}
-            label={formData.propsure_id ? 'GEO TAGGED (BETA)' : 'GEO TAGGING (BETA)'}
+            label={formData.propsure_id ? 'GEO TAGGED' : 'GEO TAGGING'}
             iconName="ios-checkmark-circle-outline"
             showIcon={ formData.propsure_id ? true: false}
             onPress={() => {

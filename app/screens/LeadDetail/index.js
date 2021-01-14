0954afaaf6cdae50034dcadfa543e310a18010ba
haris0525 <haris.amjad@graana.com>
2021-01-14 11:26:21 +0500

    removed console

--- app/screens/LeadDetail/index.js
+++ app/screens/LeadDetail/index.js
@@ -321,7 +321,6 @@ class LeadDetail extends React.Component {
       let purpose = lead.purpose === 'sale' ? 'Buy' : 'Rent'
       type = purpose
     }
-    console.log('lead: ', lead)
     return !loading ? (
       <ScrollView
         showsVerticalScrollIndicator={false}

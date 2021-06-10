a93461de93a5f009a19a50b8f855ba8a934d874f
haris0525 <haris.amjad@graana.com>
2021-06-10 17:29:12 +0500

    ARMS-1322 RCM implementation for called number

--- app/components/HistoryTile/index.js
+++ app/components/HistoryTile/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from './style';
-import AppStyles from '../../AppStyles';
-import dots from '../../../assets/img/dots.png';
-import moment from 'moment';
+/** @format */
+
+import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import styles from './style'
+import AppStyles from '../../AppStyles'
+import dots from '../../../assets/img/dots.png'
+import moment from 'moment'
 import StaticData from '../../StaticData'
 import helper from '../../helper'
 
@@ -12,35 +14,38 @@ class MeetingTile extends React.Component {
     super(props)
   }
 
-  
-
   render() {
     const { data, openStatus, editFunction, leadClosedCheck } = this.props
     let taskTypeData = []
-    data.taskType === 'meeting' ?
-      taskTypeData = StaticData.meetingStatus
-      :
-      taskTypeData = StaticData.callStatus
+    data.taskType === 'meeting'
+      ? (taskTypeData = StaticData.meetingStatus)
+      : (taskTypeData = StaticData.callStatus)
 
-    let response = data && data.response != null && data.response.replace(/_+/g, " ");
+    let response = data && data.response != null && data.response.replace(/_+/g, ' ')
 
     return (
       <TouchableOpacity onPress={() => null}>
-        <View style={[styles.mainTileView,]}>
+        <View style={[styles.mainTileView]}>
           <View style={[styles.contentView, AppStyles.flexDirectionRow]}>
             <View style={styles.border}>
-              <Text style={[AppStyles.mrTen, styles.meetingCon]}>{data.taskType} @</Text>
-              <Text style={[styles.fontBold]}>{helper.formatTime(data.start)} </Text>
-              <Text style={[styles.fontBold]}>{moment(data.date).format("MMM DD")}</Text>
+              <Text style={[AppStyles.mrTen, styles.meetingCon]}>
+                {`${data.taskType}`}
+                {data.calledNumber ? ` (${data.calledNumber})` : null}
+              </Text>
+              <Text style={[styles.fontBold]}>
+                @ {`${helper.formatTime(data.start)} ${moment(data.date).format('MMM DD')}`}{' '}
+              </Text>
             </View>
           </View>
-              <View>
-                <Text style={[styles.doneText, response != 'DNC' && styles.uperCase]}>{data.response !== null ? helper.showStatus(response) : 'Called'}</Text>
-              </View>
-            </View>
+          <View>
+            <Text style={[styles.doneText, response != 'DNC' && styles.uperCase]}>
+              {data.response !== null ? helper.showStatus(response) : 'Called'}
+            </Text>
+          </View>
+        </View>
       </TouchableOpacity>
     )
   }
 }
 
-export default MeetingTile;
+export default MeetingTile

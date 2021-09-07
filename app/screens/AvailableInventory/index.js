b286cd6d18eec281c1a6736188012518e6897c4c
haris0525 <haris.amjad@graana.com>
2021-09-07 11:52:54 +0500

    ARMS-1860 Added screen and roles

---
+++ app/screens/AvailableInventory/index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class AvailableInventory extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Available Inventory </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})

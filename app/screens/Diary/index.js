b247e875a8b43fe06e8d489ec6a1cf55ac0df152
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2021-11-26 15:35:25 +0500

    ARMS-2448: Crash Fix

--- app/screens/Diary/index.js
+++ app/screens/Diary/index.js
@@ -123,12 +123,12 @@ class Diary extends React.Component {
       }
     }
 
-    if (array.length == 2) {
+    if (array && array.length == 2) {
       const start = formatDateTime(selectedDate, array[0].armsShift.startTime)
       const end = formatDateTime(nextDay, array[1].armsShift.endTime)
 
       this.setStatsData(start, end)
-    } else if (array.length == 3) {
+    } else if (array && array.length == 3) {
       const start = formatDateTime(selectedDate, array[0].armsShift.startTime)
       const end = formatDateTime(
         array[0].armsShift.name == 'Evening' ? nextDay : selectedDate,
@@ -137,10 +137,10 @@ class Diary extends React.Component {
 
       this.setStatsData(start, end)
     } else {
-      const start = formatDateTime(selectedDate, array[0].armsShift.startTime)
+      const start = formatDateTime(selectedDate, array && array[0].armsShift.startTime)
       const end = formatDateTime(
-        array[0].armsShift.name == 'Evening' ? nextDay : selectedDate,
-        array[0].armsShift.endTime
+        array && array[0].armsShift.name == 'Evening' ? nextDay : selectedDate,
+        array && array[0].armsShift.endTime
       )
 
       this.setStatsData(start, end)

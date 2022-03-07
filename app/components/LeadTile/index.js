272e0f3549aec5e616ad2e8124f2238d2cdd13fb
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2022-03-07 16:45:39 +0500

    ARMS-3435: Title Update

--- app/components/LeadTile/index.js
+++ app/components/LeadTile/index.js
@@ -392,7 +392,7 @@ class LeadTile extends React.Component {
                     {data ? (
                       <Text style={[styles.priceText, changeColor, AppStyles.mbFive]}>
                         {purposeTab === 'invest' &&
-                          helper.capitalize(projectName != '' ? projectName : 'ANY')}
+                          helper.capitalize(projectName != '' ? projectName : 'Any Project')}
                         {data.projectType &&
                           data.projectType != '' &&
                           ` - ${helper.capitalize(data.projectType)}`}

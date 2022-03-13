51cd3de98c31eca655dce9bb92ba7e7eb9d56509
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2022-03-13 16:00:50 +0500

    ARMS-3288: Fixes and Updates

--- app/screens/TimeSlotManagement/index.js
+++ app/screens/TimeSlotManagement/index.js
@@ -191,14 +191,20 @@ function TimeSlotManagement(props) {
   const onEditSlots = (start, end, fromPicker = false) => {
     const { dispatch } = props
     const allSlots = props.allTimeSlot
+    let mySlotData = []
+    let myslots = []
+    let myisSelected = []
 
     for (var i = 0; i < allSlots.length; i++) {
       if (isTimeBetween(start, end, allSlots[i].startTime)) {
-        slotsData.push(allSlots[i])
-        slots.push(allSlots[i].id)
-        isSelected.push(allSlots[i].id)
+        mySlotData.push(allSlots[i])
+        myslots.push(allSlots[i].id)
+        myisSelected.push(allSlots[i].id)
       }
     }
+    setSlotsData(mySlotData)
+    setSlots(myslots)
+    setIsSelected(myisSelected)
 
     // if (fromPicker) {
     //   setSlotsData(slotsData)
@@ -208,8 +214,9 @@ function TimeSlotManagement(props) {
     //   setSSlots([])
     // }
 
-    setStartDate(slotsData[0].startTime)
-    setToDate(slotsData[slotsData.length - 1].endTime)
+    setStartDate(mySlotData[0].startTime)
+    setToDate(mySlotData[mySlotData.length - 1].endTime)
+
     if (props.slotDiary == null) {
       diaryData([], slots, dispatch)
     } else {
@@ -259,7 +266,7 @@ function TimeSlotManagement(props) {
     } else return false
   }
 
-  const verifyDetail = (e) => {
+  const verifyDetail = (e, myslots, mySlotData, myisSelected) => {
     const { dispatch } = props
     if (props.slotDiary == null) {
       diaryData([], slots, dispatch)
@@ -267,15 +274,16 @@ function TimeSlotManagement(props) {
       diaryData(props.slotDiary, slots, dispatch)
     }
 
-    setActualData()
+    setActualData(myslots, mySlotData, myisSelected)
   }
 
-  const setActualData = () => {
+  const setActualData = (myslots, mySlotData, myisSelected) => {
     const { dispatch } = props
-    const sortedAray = _.sortBy(slotsData, 'startTime')
+    const sortedAray = _.sortBy(mySlotData, 'startTime')
 
     const _format = 'YYYY-MM-DD'
     const date = selectedDate
+
     const nextDate = moment(selectedDate, _format).add(1, 'days').format(_format)
     const startTime = formatDateAndTime(selectedDate, sortedAray && sortedAray[0].startTime)
     const endTime = formatDateAndTime(
@@ -293,18 +301,21 @@ function TimeSlotManagement(props) {
       sortedAray && sortedAray[sortedAray.length - 1].endTime
     )
 
+    setSlotsData(mySlotData)
+    setSlots(myslots)
+    setIsSelected(myisSelected)
     setCheck(true)
     setDisabled(false)
     setTempDate(date)
     setTempEndTime(endTime)
     setTempStartTime(startTime)
-    setTempSlot(slots)
+    setTempSlot(myslots)
     setStartDate(sDate)
     setToDate(eDate)
     setTimeEnd(endTime)
     setTimeStart(startTime)
 
-    dispatch(setSlotData(date, startTime, endTime, slots))
+    dispatch(setSlotData(date, startTime, endTime, myslots))
     dispatch(setDataSlotsArray(sortedAray))
   }
 
@@ -326,7 +337,7 @@ function TimeSlotManagement(props) {
   }
 
   const onDone = () => {
-    setActualData()
+    // setActualData()
     const dataActual = []
     const _format = 'YYYY-MM-DD'
     const { dispatch, navigation, route, allTimeSlot } = props
@@ -400,28 +411,31 @@ function TimeSlotManagement(props) {
 
   const showDetailNew = (e) => {
     const tempAray = slotsData.length != 0 ? _.sortBy(slotsData, 'id') : []
+    let mySlotData = [...slotsData]
+    let myslots = [...slots]
+    let myisSelected = [...isSelected]
     if (slotsData.length == 0) {
-      slotsData.push(e)
-      slots.push(e.id)
-      isSelected.push(e.id)
+      mySlotData.push(e)
+      myslots.push(e.id)
+      myisSelected.push(e.id)
       // fortyPercent(e)
-      verifyDetail(e)
+      verifyDetail(e, myslots, mySlotData, myisSelected)
     } else {
       if (e.id == tempAray[0].id - 1 || e.id == tempAray[tempAray.length - 1].id + 1) {
-        slotsData.push(e)
-        slots.push(e.id)
-        isSelected.push(e.id)
+        mySlotData.push(e)
+        myslots.push(e.id)
+        myisSelected.push(e.id)
         // fortyPercent(e)
-        verifyDetail(e)
+        verifyDetail(e, myslots, mySlotData, myisSelected)
       } else if (_.contains(tempAray, e)) {
       } else {
         setSlotsData([e])
         setSlots([e.id])
         setTempSlot([e.id])
         setIsSelected([e.id])
-        setSSlots([])
+        // setSSlots([])
         // fortyPercent(e)
-        verifyDetail(e)
+        verifyDetail(e, [e.id], [e], [e.id])
       }
     }
   }
@@ -1021,7 +1035,7 @@ function TimeSlotManagement(props) {
           // showError={checkValidation === true && time === ''}
           errorMessage={'Required'}
           iconSource={require('../../../assets/icons/time.png')}
-          date={new Date()}
+          date={timeStart ? new Date(moment(timeStart).format()) : new Date()}
           selectedValue={timeStart ? helper.formatTime(timeStart) : ''}
           handleForm={(value, name) => handleTimeStart(value, name)}
         />
@@ -1033,7 +1047,7 @@ function TimeSlotManagement(props) {
           // showError={checkValidation === true && viewing.time === ''}
           errorMessage={'Required'}
           iconSource={require('../../../assets/icons/time.png')}
-          date={new Date()}
+          date={timeEnd ? new Date(moment(timeEnd).format()) : new Date()}
           selectedValue={timeEnd ? helper.formatTime(timeEnd) : ''}
           handleForm={(value, name) => handleTimeEnd(value, name)}
         />

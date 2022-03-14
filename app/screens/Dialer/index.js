b018f08dce6e4e01ebe167de67676dfab24f6413
haris-0525 <haris.amjad@graana.com>
2022-03-14 16:52:50 +0500

    ARMS-3190 Contact Dialer design change

--- app/screens/Dialer/index.js
+++ app/screens/Dialer/index.js
@@ -8,11 +8,8 @@ import {
   TouchableOpacity,
   Image,
   SafeAreaView,
-  TextInput,
-  TouchableWithoutFeedback,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
+  Dimensions,
+  ScrollView,
 } from 'react-native'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
@@ -20,36 +17,33 @@ import { getARMSContacts, setSelectedContact } from '../../actions/armsContacts'
 import _ from 'underscore'
 import ArmsContactTile from '../../components/ArmsContactTile'
 import AppStyles from '../../AppStyles'
-import { Feather } from '@expo/vector-icons'
 import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen'
 import fuzzy from 'fuzzy'
 import VirtualKeyboard from 'react-native-virtual-keyboard'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
+import PhoneBookContactsTile from '../../components/PhonebookContactsTile'
+import Search from '../../components/Search'
+
+const layout = Dimensions.get('window')
 
 class Dialer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      allContacts: [],
       numberTxt: '',
+      index: 0,
+      routes: [
+        { key: 'first', title: 'Keypad' },
+        { key: 'second', title: 'Phonebook' },
+      ],
+      searchText: '',
     }
   }
 
-  componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(getARMSContacts()).then((res) => {
-      const { armsContacts, contacts } = this.props
-      let newContactsArr = _.uniq(armsContacts.rows.concat(contacts), false)
-      newContactsArr = newContactsArr.map((item) => {
-        if (item.phone) {
-          return item
-        } else {
-          return {
-            ...item,
-            phone: item.phoneNumbers[0].number,
-          }
-        }
-      })
-      this.setState({ allContacts: newContactsArr })
+  onPhoneContactPress = (data) => {
+    const { navigation, dispatch } = this.props
+    dispatch(setSelectedContact(data), false).then((res) => {
+      navigation.navigate('PhoneContactDetail')
     })
   }
 
@@ -70,7 +64,7 @@ class Dialer extends Component {
         firstName: selectedContact && selectedContact.firstName ? selectedContact.firstName : '',
         lastName: selectedContact && selectedContact.lastName ? selectedContact.lastName : '',
       }
-      console.log('body=>', body)
+      // console.log('body=>', body)
       dispatch(setSelectedContact(body, true)).then((res) => {
         navigation.replace('ContactFeedback')
       })
@@ -79,34 +73,33 @@ class Dialer extends Component {
     }
   }
 
-  render() {
-    const { allContacts, numberTxt } = this.state
-    const { navigation, dispatch, permissions } = this.props
+  FirstRoute = () => {
+    const { numberTxt } = this.state
+    const { armsContacts } = this.props
     let data = []
     if (numberTxt !== '' && data && data.length === 0) {
-      data = fuzzy.filter(numberTxt, allContacts, {
+      data = fuzzy.filter(numberTxt, armsContacts.rows, {
         extract: (e) => (e.phone ? e.phone : ''),
       })
       data = data.map((item) => item.original)
     }
-
     return (
-      <SafeAreaView style={AppStyles.containerWithoutPadding}>
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <ArmsContactTile
-              data={item}
-              onPress={(data) => this.onContactPress(data)}
-              showCallButton={false}
-            />
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-
+      <View style={AppStyles.containerWithoutPadding}>
+        <ScrollView
+          style={{ minHeight: heightPercentageToDP('20%'), maxHeight: heightPercentageToDP('40%') }}
+        >
+          {data &&
+            data.map((item) => (
+              <ArmsContactTile
+                key={item.id.toString()}
+                data={item}
+                onPress={(data) => this.onContactPress(data)}
+                showCallButton={false}
+              />
+            ))}
+        </ScrollView>
         <View style={styles.underLine} />
-
-        <View style={{ paddingVertical: 20 }}>
+        <View style={{ paddingVertical: 10 }}>
           <Text style={styles.inputStyle}>{this.state.numberTxt}</Text>
           <VirtualKeyboard
             color="black"
@@ -114,45 +107,111 @@ class Dialer extends Component {
             onPress={(val) => this.setState({ numberTxt: val })}
           />
         </View>
+      </View>
+    )
+  }
 
+  SecondRoute = () => {
+    const { searchText } = this.state
+    const { contacts } = this.props
+    let data = []
+    if (searchText !== '' && data && data.length === 0) {
+      data = fuzzy.filter(searchText, contacts, {
+        extract: (e) => (e.firstName ? e.firstName + ' ' + e.lastName : ''),
+      })
+      data = data.map((item) => item.original)
+    } else {
+      data = contacts
+    }
+    return (
+      <View style={AppStyles.containerWithoutPadding}>
+        <Search
+          placeholder={'Search Contacts'}
+          searchText={searchText}
+          setSearchText={(text) => this.setState({ searchText: text })}
+        />
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <PhoneBookContactsTile data={item} onPress={(data) => this.onPhoneContactPress(data)} />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
+    )
+  }
+
+  renderScene = SceneMap({
+    first: this.FirstRoute,
+    second: this.SecondRoute,
+  })
+
+  renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: 'white' }}
+      tabStyle={{
+        backgroundColor: 'white',
+        borderTopColor: AppStyles.colors.subTextColor,
+        borderTopWidth: 0.2,
+      }}
+      renderLabel={({ route, focused, color }) => (
+        <Text
+          style={{
+            color: focused ? AppStyles.colors.primaryColor : AppStyles.colors.textColor,
+            fontFamily: AppStyles.fonts.defaultFont,
+            fontSize: 12,
+            margin: 5,
+          }}
+        >
+          {route.title}
+        </Text>
+      )}
+      renderIcon={({ route, focused, color }) =>
+        route.key === 'first' ? (
+          <Image
+            source={
+              focused
+                ? require(`../../../assets/img/dial_pad_blue.png`)
+                : require(`../../../assets/img/dial_pad.png`)
+            }
+          />
+        ) : (
+          <Image
+            source={
+              focused
+                ? require(`../../../assets/img/phonebook_blue.png`)
+                : require(`../../../assets/img/phonebook.png`)
+            }
+          />
+        )
+      }
+    />
+  )
+
+  render() {
+    const { index, routes } = this.state
+    const { navigation, dispatch, permissions } = this.props
+
+    return (
+      <View style={{ flex: 1 }}>
         <TouchableOpacity style={styles.callButton} onPress={() => this.callNumber()}>
           <Image source={require(`../../../assets/img/dialer_call.png`)} />
         </TouchableOpacity>
-
-        <View style={styles.bottomTab}>
-          <TouchableOpacity
-            style={styles.bottomTabViews}
-            onPress={() => console.log('focus to control')}
-          >
-            <Image source={require(`../../../assets/img/dial_pad.png`)} />
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.bottomTabViews}
-            onPress={() => navigation.navigate('PhoneContacts')}
-          >
-            <Image source={require(`../../../assets/img/phonebook.png`)} />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+        <TabView
+          renderTabBar={this.renderTabBar}
+          navigationState={{ index, routes }}
+          renderScene={this.renderScene}
+          onIndexChange={(value) => this.setState({ index: value })}
+          initialLayout={{ width: layout.width }}
+          tabBarPosition={'bottom'}
+        />
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  bottomTab: {
-    height: heightPercentageToDP('10%'),
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: 'white',
-    borderTopWidth: 0.5,
-    borderTopColor: AppStyles.colors.subTextColor,
-  },
-  bottomTabViews: {
-    width: '50%',
-    alignItems: 'center',
-  },
   inputStyle: {
     color: AppStyles.colors.primaryColor,
     fontFamily: AppStyles.fonts.defaultFont,
@@ -164,12 +223,13 @@ const styles = StyleSheet.create({
   callButton: {
     zIndex: 10,
     elevation: 10,
-    marginBottom: -30,
+    position: 'absolute',
+    bottom: 40,
     alignSelf: 'center',
     justifyContent: 'flex-end',
   },
   underLine: {
-    height: 0.5,
+    height: 0.2,
     width: '100%',
     backgroundColor: AppStyles.colors.subTextColor,
   },

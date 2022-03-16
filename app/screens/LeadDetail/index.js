c84dde6e34234d64f38a53a2730cfdf448c59e24
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2022-03-16 12:02:16 +0500

    ARMS-3398: Detail Changes

--- app/screens/LeadDetail/index.js
+++ app/screens/LeadDetail/index.js
@@ -464,7 +464,9 @@ class LeadDetail extends React.Component {
               <Text style={styles.headingText}>Requirement </Text>
               <Text style={styles.labelText}>
                 {!lead.projectId && leadSize}
-                {!lead.projectId && `${helper.capitalize(lead.subtype)} to ${type}`}
+                {!lead.projectId && type !== 'Investment'
+                  ? `${helper.capitalize(lead.subtype)} to ${type}`
+                  : `Looking to Invest in Any Project`}
                 {lead.projectId && (lead.projectType ? helper.capitalize(lead.projectType) : '-')}
               </Text>
             </View>

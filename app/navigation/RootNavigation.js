f6f8ec63c21fc0a8063b99db813b8c685eea5e9b
Maria-Graana <102798842+Maria-Graana@users.noreply.github.com>
2022-04-12 12:00:01 +0500

    ARMS-3437

--- app/navigation/RootNavigation.js
+++ app/navigation/RootNavigation.js
@@ -6,6 +6,11 @@ export function navigate(name) {
   navigationRef.current?.navigate(name, {screen: name});
 }
 
+
+export function navigateToSpecificTab(name, tab) {
+  navigationRef.current?.navigate(name, { screen: tab})
+}
+
 export function navigateTo(name, params) {
   navigationRef.current?.navigate(name, params);
 }

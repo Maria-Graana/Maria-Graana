989d12ee2d3bdbde1d12e9cad8c79d969023b6d1
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2022-04-15 15:11:17 +0500

    DM-143: Fix

--- app/screens/Landing/index.js
+++ app/screens/Landing/index.js
@@ -21,7 +21,7 @@ import { FAB } from 'react-native-paper'
 import UpdateApp from '../../UpdateApp'
 import styles from './style'
 import { clearDiaries } from '../../actions/diary'
-import { setSlotData } from '../../actions/slotManagement'
+import { getTimeShifts, setSlotData } from '../../actions/slotManagement'
 import moment from 'moment'
 
 const _format = 'YYYY-MM-DD'
@@ -78,6 +78,7 @@ class Landing extends React.Component {
 
   async componentDidMount() {
     const { navigation, dispatch } = this.props
+    dispatch(getTimeShifts())
     this._unsubscribe = navigation.addListener('focus', () => {
       dispatch(getListingsCount())
       dispatch(isTerminalUser())

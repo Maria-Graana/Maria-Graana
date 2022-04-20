3774f1974b225457e54ff6064e54525218707cbe
haris-0525 <haris.amjad@graana.com>
2022-04-20 16:11:00 +0500

    Update index.js

--- app/screens/AddCMLead/index.js
+++ app/screens/AddCMLead/index.js
@@ -147,7 +147,6 @@ class AddCMLead extends Component {
         CMLead.projectId = null
         CMLead.projectName = null
       }
-      console.log(CMLead)
       this.setState({ loading: true }, () => {
         if (update) {
           // update lead

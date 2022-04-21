d98621a1e6a1c2438d66eb1d951c76264c3ba013
Maria-Graana <102798842+Maria-Graana@users.noreply.github.com>
2022-04-21 16:00:27 +0500

    permision changings

--- app/LocalNotifications.js
+++ app/LocalNotifications.js
@@ -1,23 +1,20 @@
-
+/** @format */
 
 import Constants from 'expo-constants'
 import * as Notifications from 'expo-notifications'
-import * as Permissions from 'expo-permissions'
 import moment from 'moment-timezone'
 import { Keyboard } from 'react-native'
 
 const submitNotification = (body, date) => {
   Keyboard.dismiss()
-  const trigger = convertTimeZone(date);
-  
+  const trigger = convertTimeZone(date)
   let localNotification = {
-    title:` ${body.title} ${body.clientName?  "with "+body.clientName : ''}`,
+    title: ` ${body.title} ${body.clientName ? "with " + body.clientName : ''}`,
     body: body.body,
     data: {
       type: 'local',
       date: date,
       id: body.id,
-      
     },
     sound: 'default',
   }
@@ -32,7 +29,7 @@ const submitNotification = (body, date) => {
 const convertTimeZone = (date) => {
   let _format = 'YYYY-MM-DDTHH:mmZ'
   let paktz = moment(date).format(_format)
-  var duration = moment.duration({ minutes:15 })
+  var duration = moment.duration({ minutes: 15 })
   var sub = moment(paktz, _format).subtract(duration).format()
   return new Date(sub)
 }
@@ -43,10 +40,10 @@ const handleNotification = () => {
 
 const askNotification = async (body, date) => {
   if (Constants.isDevice) {
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
     let finalStatus = existingStatus
     if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+      const { status } = await Notifications.getPermissionsAsync()
       finalStatus = status
     }
     if (finalStatus !== 'granted') {
@@ -59,7 +56,6 @@ const askNotification = async (body, date) => {
 }
 
 const TimerNotification = (body, date) => {
-
   askNotification(body, date)
 }
 

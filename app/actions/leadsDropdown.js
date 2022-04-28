52b6807fe1b18752d5343df7e4f5be3c746de49b
Maria-Graana <102798842+Maria-Graana@users.noreply.github.com>
2022-04-28 16:09:59 +0500

    remove console

--- app/actions/leadsDropdown.js
+++ app/actions/leadsDropdown.js
@@ -3,7 +3,7 @@ import * as types from '../types';
 import * as Contacts from 'expo-contacts';
 
 export function setLeadsDropdown(value) {
-    console.log("Actions",value)
+   
     return (dispatch, getsState) => {
     dispatch({
         type: types.COUNTER_CHANGE,

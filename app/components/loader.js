0e756a39c9f8ca2cfe4b5073a880452bc95d2bb0
AreebSiddiqui-Graana <76466564+AreebSiddiqui-Graana@users.noreply.github.com>
2022-06-03 18:12:47 +0500

    ARMS-4137: Lottie Animation Added

--- app/components/loader.js
+++ app/components/loader.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Image, View, ActivityIndicator } from 'react-native'
 import AppStyles from '../AppStyles'
+import LottieView from 'lottie-react-native'
 
 class Loader extends React.Component {
   constructor(props) {
@@ -15,7 +16,13 @@ class Loader extends React.Component {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         {loading == true ? (
-          <ActivityIndicator size={sizeLabel} color={loaderColor} />
+          // <ActivityIndicator size={sizeLabel} color={loaderColor} />
+          <LottieView
+            autoPlay
+            loop
+            source={require('../../assets/json/lf30_editor_angaivhu.json')}
+            style={{ height: 100, width: 100 }}
+          />
         ) : (
           // <Image source={require('../../assets/loader.gif')} style={{ width: 60, height: 60 }} />
           <Image

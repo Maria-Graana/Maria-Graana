c2f0537bfd60ca93aaf6482c944b5ee22793b227
haris0525 <haris.amjad@graana.com>
2022-06-09 15:56:08 +0500

    sdk-44 changes

--- app/config.js
+++ app/config.js
@@ -1,10 +1,8 @@
 /** @format */
-
 import * as Updates from 'expo-updates'
 
-const config = {
+const ENV = {
   development: {
-    // apiPath: 'https://stage.arms.graana.rocks',
     apiPath: 'https://dev.arms.graana.rocks',
     graanaUrl: 'https://dev.graana.rocks',
     channel: 'development',
@@ -21,14 +19,15 @@ const config = {
   },
 }
 
-if (Updates.releaseChannel === 'default') {
-  module.exports = config['development']
-} else if (Updates.releaseChannel.indexOf('production') !== -1) {
-  module.exports = config['production']
-} else if (Updates.releaseChannel.indexOf('staging') !== -1) {
-  module.exports = config['staging']
-} else if (Updates.releaseChannel.indexOf('development') !== -1) {
-  module.exports = config['development']
-} else {
-  module.exports = config['development']
+function getEnvVars(env = '') {
+  if (env && env.indexOf('staging') !== -1) return ENV.staging
+  else if (env && env.indexOf('production') !== -1) return ENV.production
+  else return ENV.development
+}
+
+const config = getEnvVars(Updates.releaseChannel)
+export default config
+
+export function getEndPoint(endPoint) {
+  return config.apiPath
 }
